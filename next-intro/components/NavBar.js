2c1ec8eaf6de42d2aca5950575af9923f305589d
Eunhyung Choi <milkiss0414@naver.com>
2022-11-02 17:55:08 +0900

    Feat: css modules

--- next-intro/components/NavBar.js
+++ next-intro/components/NavBar.js
@@ -1,17 +1,18 @@
 import Link from "next/link";
-import {useRouter} from "next/router"
+import {useRouter} from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
 
   return (
-    <div>
+    <nav className={styles.nav}>
       <Link href="/">
         <a style={{ color: router.pathname === "/" ? "blue": "black"}}>Home</a>
       </Link>
       <Link href="/about">
         <a style={{ color: router.pathname === "/about" ? "blue": "black"}}>About</a>
       </Link>
-    </div>
+    </nav>
   )
 }
